5e7bf317ed0c2783e74c5aad85a193cfdb8f2e6b
Viru Thanthri <33057902+ruv1ru@users.noreply.github.com>
2018-10-27 18:34:25 +1100

    Update Page.cs

--- OneToMany/Page.cs
+++ OneToMany/Page.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 public class Page : BaseEntity
 {
     public virtual int Number { get; set; }
+    
+    public virtual int BookId { get; set; }
+    
+    public virtual Book Book { get; set; }
 
 }
