c4c9076424fb496df9855c24c5862b7105dbee9b
Viru Thanthri <33057902+ruv1ru@users.noreply.github.com>
2018-10-27 11:11:05 +1100

    Update CreateStudentTable.sql

--- ManyToMany/CreateStudentTable.sql
+++ ManyToMany/CreateStudentTable.sql
@@ -2,5 +2,6 @@ CREATE TABLE dbo.Student (
 	Id int IDENTITY(1,1) NOT NULL,
 	FirstName nvarchar(100) NOT NULL,
 	LastName nvarchar(100) NOT NULL,
-	CreatedOnUtc datetime NOT NULL 
+	CreatedOnUtc datetime NOT NULL ,
+	CONSTRAINT PK_Student PRIMARY KEY (Id)
 )
